21304799f87968da00ab89cb666039c0646c1f3c
Jerry C <jerrycardenasr@gmail.com>
2020-03-19 18:54:04 -0600

    Nodo2

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -6,7 +6,8 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleGame<Celda> game = new ConsoleGame<Celda>(6, 7, 5);
+            Console.ReadKey();
         }
     }
 }
